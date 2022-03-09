dded7e391cca872b9b3948855110366c06e90664
patelhar <harshyboi2004@gmail.com>
2022-03-09 09:45:21 -0600

    AI kind of works

--- src/BoardCore.java
+++ src/BoardCore.java
@@ -51,4 +51,52 @@ public class BoardCore {
             return false;
         }
     }
+
+    public void shifterAIRun(Queens queenHandler){
+        BoardCore coreBoard = this;
+        int queenRequirement = queenHandler.getQueenCount() + 1;
+        int xShifter = 0;
+        int yShifter = 0;
+        boolean shiftY = true;
+        int previousQueens = queenHandler.getQueenCount();
+        coreBoard.setEmpty();
+        queenHandler.resetQueens(coreBoard);
+        while (queenHandler.getQueenCount() < queenRequirement) {
+            if (shiftY) {
+                yShifter++;
+            } else {
+                xShifter++;
+            }
+
+            for (int i = xShifter; i < coreBoard.getWidth(); i++) { //shift everything over
+                for (int j = yShifter; j < coreBoard.getHeight(); j++) {
+                    coreBoard.setBoard(queenHandler.placeQueen(i,j));
+                    queenHandler.updateQueens(coreBoard);
+                }
+            }
+            coreBoard.printBoard();
+            System.out.println("Shifted board");
+            for (int i = 0; i < coreBoard.getWidth(); i++) { //put queen in new spot
+                for (int j = 0; j < coreBoard.getHeight(); j++) {
+                    coreBoard.setBoard(queenHandler.placeQueen(i,j));
+                    queenHandler.updateQueens(coreBoard);
+                }
+            }
+            coreBoard.printBoard();
+            System.out.println("Got " + queenHandler.getQueenCount() +"\tyShifter is "+yShifter+"\txShifter is "+xShifter);
+            if (yShifter >= width || xShifter >= height) {
+                if (yShifter <= width) {
+                    xShifter = height;
+                    yShifter++;
+                } else {
+                    if (previousQueens == queenHandler.getQueenCount() && !shiftY) {
+                        break;
+                    } else if (previousQueens == queenHandler.getQueenCount()) {
+                        shiftY = false;
+                        yShifter = 0;
+                    }
+                }
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -21,27 +21,11 @@ public class Main {
         coreBoard.printBoard();
         System.out.println("Got " + queenHandler.getQueenCount() + " queens\nType 1 to repeat and try for " + (queenHandler.getQueenCount()+1) + " queens");
         int response = userInput.nextInt();
-        if (response == 1) {
-            int queenRequirement = queenHandler.getQueenCount() + 1;
-            int xShifter = 0;
-            int yShifter = 0;
-            coreBoard.setEmpty();
-            queenHandler.resetQueens(coreBoard);
-            while (queenHandler.getQueenCount() < queenRequirement) {
-                yShifter++;
-                for (int i = xShifter; i < coreBoard.getWidth(); i++) {
-                    for (int j = yShifter; j < coreBoard.getHeight(); j++) {
-                        coreBoard.setBoard(queenHandler.placeQueen(i,j));
-                        queenHandler.updateQueens(coreBoard);
-                    }
-                }
-                coreBoard.printBoard();
-                System.out.println("Got " + queenHandler.getQueenCount() +"\tShifter is "+yShifter);
-                if (yShifter > 100) {
-                    break;
-                }
 
-            }
+        while (response == 1) {
+            coreBoard.shifterAIRun(queenHandler);
+            System.out.println("Got " + queenHandler.getQueenCount() + " queens\nType 1 to repeat and try for " + (queenHandler.getQueenCount()+1) + " queens");
+            response = userInput.nextInt();
         }
 
 
